ddf8a798cf210066ca2a32355fb951d29b0dde15
Eugene <84244694+Endocryne@users.noreply.github.com>
2022-06-28 20:40:05 +0300

    Update Builder.h

--- Builder.h
+++ Builder.h
@@ -5,15 +5,21 @@
 #include <memory>
 #include "Element.h"
 
-struct Builder
-{
+struct Builder {
     Element root;
-    Builder(std::string root_name) { root.name = root_name; }
+    Builder(std::string root_name) {
+        root.name = root_name;
+    }
 
     Builder add_child(std::string child_name, std::string child_text) {
-        Element e{child_name, child_text};
+        Element e {
+            child_name,
+            child_text
+        };
         root.elements.emplace_back(e);
         return *this;
     }
-    std::string str() const { return root.str(); }
+    std::string str() const {
+        return root.str();
+    }
 };
